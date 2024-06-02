d0d7c3a126eaf20d9441ac987ebc799ddeb42ccd
RobsonFe <robson_20ferreira@hotmail.com>
2024-06-01 21:39:11 -0300

    update:Views

---
+++ courses/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import Course, Assessment
+
+class AssessmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        extra_kwargs = {
+            'email': {'write_only': True}
+        }
+        model = Assessment
+        fields = {
+            'id',
+            'course',
+            'name',
+            'email',
+            'comment',
+            'create',
+            'active'
+        }
+
+class CourseSerializers(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Course
+        fields ={
+            'id',
+            'title',
+            'url',
+            'create',
+            'active'
+        }
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import response
+from rest_framework import status
+from .models import Course, Assessment
+from .serializers import CourseSerializers, AssessmentSerializer
 
-# Create your views here.
+class CourseAPIView(APIView):
+
+    def get(self, request):
+        course = Course.objects.all()
+        serializer = CourseSerializers(course, many=True)
+        return response(serializer.data)
+
+class AssessmentAPIView(APIView):
+    
+    def get(self, request):
+        assessment = Assessment.objects.all()
+        serializer = AssessmentSerializer(assessment, many=True)
+        return response(serializer.data)
