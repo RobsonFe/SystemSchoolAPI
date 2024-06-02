caa88f3a480cf368b09964197f273615c79e9eb2
RobsonFe <robson_20ferreira@hotmail.com>
2024-06-02 14:25:00 -0300

    update:CRUDCompleteAPI

--- courses/urls.py
+++ courses/urls.py
@@ -3,5 +3,7 @@ from .views import CourseAPIView, AssessmentAPIView
 
 urlpatterns = [
     path('courses/', CourseAPIView.as_view(), name='courses'),
-    path('assessment/', AssessmentAPIView.as_view(), name='assessment')
+    path('courses/<int:pk>/', CourseAPIView.as_view(), name='course-detail'),
+    path('assessments/', AssessmentAPIView.as_view(), name='assessments'),
+    path('assessments/<int:pk>/', AssessmentAPIView.as_view(), name='assessment-detail'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Course, Assessment
 from .serializers import CourseSerializer, AssessmentSerializer
+from django.shortcuts import get_object_or_404
 
 class CourseAPIView(APIView):
 
@@ -19,6 +20,27 @@ class CourseAPIView(APIView):
     #   return Response({"msg": "Criado com sucesso"}, status=status.HTTP_201_CREATED)
     #   return Response({"id": serializer.data['id'], "curso": serializer.data['title']}, status=status.HTTP_201_CREATED)
         
+    def put(self, request, pk):
+            course = get_object_or_404(Course, pk=pk)
+            serializer = CourseSerializer(course, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def patch(self, request, pk):
+        course = get_object_or_404(Course, pk=pk)
+        serializer = CourseSerializer(course, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def delete(self, request, pk):
+        try:
+            course = Course.objects.get(pk=pk)
+            course.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Course.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
 class AssessmentAPIView(APIView):
     
@@ -32,3 +54,25 @@ class AssessmentAPIView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    def put(self, request, pk):
+        assessment = get_object_or_404(Assessment, pk=pk)
+        serializer = AssessmentSerializer(assessment, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def patch(self, request, pk):
+        assessment = get_object_or_404(Assessment, pk=pk)
+        serializer = AssessmentSerializer(assessment, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def delete(self, request, pk):
+        try:
+            assessment = Assessment.objects.get(pk=pk)
+            assessment.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Assessment.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
