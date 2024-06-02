163623430f249c7deee7acb6c4fcc9df79f4a1cc
RobsonFe <robson_20ferreira@hotmail.com>
2024-06-01 23:42:34 -0300

    update:postMethod

--- courses/views.py
+++ courses/views.py
@@ -11,9 +11,24 @@ class CourseAPIView(APIView):
         serializer = CourseSerializer(course, many=True)
         return Response(serializer.data)
 
+    def post(self, request):
+        serializer = CourseSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #   return Response({"msg": "Criado com sucesso"}, status=status.HTTP_201_CREATED)
+    #   return Response({"id": serializer.data['id'], "curso": serializer.data['title']}, status=status.HTTP_201_CREATED)
+        
+
 class AssessmentAPIView(APIView):
     
     def get(self, request):
         assessment = Assessment.objects.all()
         serializer = AssessmentSerializer(assessment, many=True)
         return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = AssessmentSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
